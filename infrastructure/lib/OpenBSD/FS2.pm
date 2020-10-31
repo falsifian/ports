dc636a1c1f045d12ffe92b197861ec37d37bca5b
Greg Steuck <greg@nest.cx>
2020-10-30 23:57:38 -0700

    Remove GhcConf packing element as ghc-pkg-recache is no more
    These were previously used to automatically add tags to PLIST for
    Haskell library ports. Since library ports no longer exist, such
    tagging only ever triggers for ghc package where it is now harmful.

--- infrastructure/lib/OpenBSD/FS2.pm
+++ infrastructure/lib/OpenBSD/FS2.pm
@@ -67,7 +67,6 @@ sub classes
 		OpenBSD::FS::File::Glib2Schema
 		OpenBSD::FS::File::PkgConfig
 		OpenBSD::FS::File::MimeInfo
-		OpenBSD::FS::File::GhcConf
 		OpenBSD::FS::File::Icon
 		OpenBSD::FS::File::IconTheme
 		OpenBSD::FS::File::Subinfo OpenBSD::FS::File::Info
@@ -232,19 +231,6 @@ sub recognize
 sub element_class
 {
 	'OpenBSD::PackingElement::MimeInfo';
-}
-
-package OpenBSD::FS::File::GhcConf;
-our @ISA = qw(OpenBSD::FS::File);
-sub recognize
-{
-	my ($class, $filename, $fs) = @_;
-	return $filename =~ m,lib/ghc/package\.conf\.d/.*\.conf$,;
-}
-
-sub element_class
-{
-	'OpenBSD::PackingElement::GhcConf';
 }
 
 package OpenBSD::FS::File::IconThemeDirectory;
